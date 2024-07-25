2a2c19bc953b735f04842fa12318746f109476df
deltaforce26 <ahron052715@gmail.com>
2024-07-25 17:46:51 +0300

    Add project files.

---
+++ CarModel/Controllers/CarController.cs
@@ -0,0 +1,25 @@
+﻿using CarModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarModel.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Model(string year, string name)
+        {
+            Car car = new Car
+            {
+
+                Name = string.IsNullOrEmpty(name) ? "No model" : name,
+                Year = string.IsNullOrEmpty(year) ? "N/A" : year
+            };
+            return View(car);
+        }
+        
+    }
+}
---
+++ CarModel/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarModel.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Year { get; set; }
+    }
+}
